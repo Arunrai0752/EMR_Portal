d9fb391f2d4d09b5a4edefb7fed3f83ccf183c33
Arunrai0752 <realarunrai0752@gmail.com>
2025-07-23 00:28:25 +0530

    Update Home and Login components with styling and functionality improvements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <>
-          <main className='   h-[90vh] w-full bg-blue-400'>
+          <main className=' h-[90vh] w-full bg-blue-500/40'>
 
             
          
